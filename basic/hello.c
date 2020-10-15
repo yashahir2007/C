95b61762f097ac271fe755135373c0731ad7932d
Abhishek Sharma <58878438+imgeekabhi@users.noreply.github.com>
2020-10-15 09:37:26 +0530

    Update hello.c

--- basic/hello.c
+++ basic/hello.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 #include <string.h>
-#include <math.h>
-#include <stdlib.h>
 
 int main() {
-   char input_string[105]; // Declare a variable named 'input_string' to hold our input.
-     
+   char input_string[101]; // Declare a variable named 'input_string' to hold user input.
+   printf("Enter the string :- ");  
    scanf("%[^\n]", input_string); // Read a full line of input from stdin and save it to our variable, input_string.
      
    //Your first line of output goes here
-     printf("Hello, World.\n"); 
-     printf("%s\n",input_string);
+     printf("Hello, \n"); 
+     printf("\t%s\n",input_string);
     
     // TODO: Write a line of code here that prints the contents of input_string to stdout.
     
